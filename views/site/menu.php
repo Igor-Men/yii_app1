f55add0adfcd0a952b5859208e779016da2c393b
igor <dp030988kie@gmail.com>
2016-02-07 17:52:10 +0200

    menu повесил

---
+++ views/site/menu.php
@@ -0,0 +1,172 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: igor
+ * Date: 07.02.16
+ * Time: 17:47
+ */
+?>
+
+<!--middle content-->
+<div id="bd">
+
+    <div class="yui-g main-box">
+
+        <div class="top-tagline">
+            <p>Suspendisse dictum suscipit odio. Nulla dui arcu, fermentum quis, mattis quis, ultricies id, justo. Nulla facilisi. Proin tortor sem, vehicula vitae, facilisis vel</p>
+        </div><!--end top tagline -->
+
+        <div class="breadcrumbs">
+            You are here:
+            <a href="index.html" style="color:#561c40; font-weight:bold;">Home</a> &raquo; <a href="menu.html">Menu</a>
+        </div><!--end breadcrumbs-->
+
+    </div><!--end yui-g main-box-->
+
+
+    <div class="yui-g main-box">
+
+        <div class="content">
+
+            <h1>Menu</h1>
+
+            <div class="page">
+
+                <div class="menu-item">
+
+                    <h3>Breakfast</h3>
+
+                    <div class="menu-item-content">
+
+                        <div class="menu-image">
+                            <a class="lightbox" href="assets/post-image.jpg"><img src="assets/menu-item-image.jpg" width="298" height="138" alt="Menu item" /></a>
+                            <p>Image No.1</p>
+                            <a href="assets/post-image.jpg" class="zoom-images lightbox">zoom <span>[+]</span></a>
+                        </div><!--end menu-image-->
+
+                        <div class="menu-desc">
+                            <h3>Lorem ipsum dolor sit</h3>
+                            <p>Suspendisse dictum suscipit odio. Nulla dui arcu, fermentum quis, mattis quis, ultricies id, justo. Nulla facilisi. Proin tortor sem, vehicula vitae, facilisis vel, accumsan a, sapien. Vivamus convallis rutrum lorem. Praesent massa lectus, vulputate sed. vestibulum eu, vestibulum eu, dolor. Integer ante. Donec imperdiet.
+                            </p>
+                        </div><!--end menu-desc -->
+
+                    </div><!--end menu-item-content-->
+
+                </div><!--end menu-item-->
+
+                <div class="menu-item">
+
+                    <h3>Lunch</h3>
+
+                    <div class="menu-item-content">
+
+                        <div class="menu-image">
+                            <a class="lightbox" href="assets/post-image.jpg"><img src="assets/menu-item-image.jpg" width="298" height="138" alt="Menu item" /></a>
+                            <p>Image No.2</p>
+                            <a href="assets/post-image.jpg" class="zoom-images lightbox">zoom <span>[+]</span></a>
+                        </div><!--end menu-image-->
+
+                        <div class="menu-desc">
+                            <h3>Lorem ipsum dolor sit</h3>
+                            <p>Suspendisse dictum suscipit odio. Nulla dui arcu, fermentum quis, mattis quis, ultricies id, justo. Nulla facilisi. Proin tortor sem, vehicula vitae, facilisis vel, accumsan a, sapien. Vivamus convallis rutrum lorem. Praesent massa lectus, vulputate sed. vestibulum eu, vestibulum eu, dolor. Integer ante. Donec imperdiet.
+                            </p>
+                        </div><!--end menu-desc -->
+
+                    </div><!--end menu-item-content-->
+
+                </div><!--end menu-item-->
+
+                <div class="menu-item">
+
+                    <h3>Dinner</h3>
+
+                    <div class="menu-item-content">
+
+                        <div class="menu-image">
+                            <a class="lightbox" href="assets/post-image.jpg"><img src="assets/menu-item-image.jpg" width="298" height="138" alt="Menu item" /></a>
+                            <p>Image No.3</p>
+                            <a href="assets/post-image.jpg" class="zoom-images lightbox">zoom <span>[+]</span></a>
+                        </div><!--end menu-image-->
+
+                        <div class="menu-desc">
+                            <h3>Lorem ipsum dolor sit</h3>
+                            <p>Suspendisse dictum suscipit odio. Nulla dui arcu, fermentum quis, mattis quis, ultricies id, justo. Nulla facilisi. Proin tortor sem, vehicula vitae, facilisis vel, accumsan a, sapien. Vivamus convallis rutrum lorem. Praesent massa lectus, vulputate sed. vestibulum eu, vestibulum eu, dolor. Integer ante. Donec imperdiet.
+                            </p>
+                        </div><!--end menu-desc -->
+
+                    </div><!--end menu-item-content-->
+
+                </div><!--end menu-item-->
+
+
+                <ul class="pagination">
+                    <li><a class="previous" href="#">Previous</a></li>
+                    <li><a class="current" href="#">1</a></li>
+                    <li><a href="#">2</a></li>
+                    <li><a href="#">3</a></li>
+                    <li><a href="#">4</a></li>
+                    <li><a href="#">5</a></li>
+                    <li><a class="next" href="#">Next</a></li>
+                </ul><!-- end pagination -->
+
+
+            </div><!-- end page -->
+        </div> <!-- end content -->
+
+        <div class="sidebar">
+            <div class="sidebar-item">
+                <ul class="social-icons">
+                    <li><a class="twitter" href="#"></a></li>
+                    <li><a class="facebook" href="#"></a></li>
+                    <li><a class="linkedin" href="#"></a></li>
+                    <li><a class="rss" href="#"></a></li>
+                </ul><!-- end social-icons-->
+            </div>
+
+            <div class="sidebar-item">
+                <div class="book-table-sidebar">
+                    <h3>Book a Table</h3>
+                    <p>Suspendisse dictum suscipit dictum suscipit.</p>
+                    <a href="book-table.html"><img src="images/book-now-sidebar.jpg" width="137" height="38" alt="Book Now!" /></a>
+                </div><!--end book-table-sidebar-->
+            </div>
+
+            <div class="sidebar-item">
+                <h3>Menu Categories</h3>
+                <ol class="menu-categories">
+                    <li><a href="#">Appetizers</a></li>
+                    <li><a href="#">Breakfast</a></li>
+                    <li><a href="#">Lunch</a></li>
+                    <li><a href="#">Dinner</a></li>
+                    <li><a href="#">Quisque</a></li>
+                    <li><a href="#">Suspendisse</a></li>
+                </ol><!-- end menu-categories-->
+            </div>
+
+            <div class="sidebar-item">
+                <ul class="download">
+                    <li><img src="images/icon-pdf.png" alt="Download a PDF" title="Image download icon" width="53" height="65" /></li>
+                    <li><p>Download a <strong>PDF</strong> version of our menu from <a href="#">here</a>.</p></li>
+                </ul><!-- end download-->
+            </div>
+
+            <div class="sidebar-item">
+                <div class="cafeteria-hours">
+                    <h3>Cafeteria Hours</h3>
+                    <ul class="cafeteria-hours-content">
+                        <li><h3 class="shedule-title">Breakfast</h3></li>
+                        <li><label>Monday - Friday</label><span>11:00 am - 03:00 pm</span></li>
+                        <li><label>Monday - Friday</label><span>11:00 am - 03:00 pm</span></li>
+                    </ul>
+                    <ul class="cafeteria-hours-content">
+                        <li><h3 class="shedule-title">Breakfast</h3></li>
+                        <li><label>Monday - Friday</label><span>11:00 am - 03:00 pm</span></li>
+                        <li><label>Monday - Friday</label><span>11:00 am - 03:00 pm</span></li>
+                    </ul>
+                </div><!-- end cafeteria-hours-->
+            </div>
+
+        </div><!-- end sidebar -->
+    </div><!-- end yui-g main-box -->
+
+</div><!-- end bd -->
