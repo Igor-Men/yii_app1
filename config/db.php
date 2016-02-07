7cfc00f116ea8efaf25035c0562256a76cf5344e
igor <dp030988kie@gmail.com>
2016-02-07 22:18:47 +0200

    issue create admin controller and layout for admin.

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=yiiapp1',
     'username' => 'root',
-    'password' => '',
+    'password' => 'userpassword',
     'charset' => 'utf8',
 ];
---
+++ controllers/AdminController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\controllers;
+
+class AdminController extends \yii\web\Controller
+{
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+
+    public $layout = 'admin_index';
+
+}
