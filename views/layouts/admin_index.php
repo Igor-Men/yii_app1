a15048bea8fe74db2fcea62d9c01427cd4885b19
igor <dp030988kie@gmail.com>
2016-02-08 22:00:02 +0200

    issue #12112 - reformvat assets

--- views/layouts/admin_index.php
+++ views/layouts/admin_index.php
@@ -1,13 +1,19 @@
 <?php
 use yii\helpers\Html;
+use yii\bootstrap\BootstrapAsset;
+use yii\bootstrap\BaseHtml;
 use yii\bootstrap\Nav;
 use yii\bootstrap\NavBar;
 use yii\widgets\Breadcrumbs;
-use app\assets\AppAsset;
+use app\assets\AppAssetAdmin;
 use yii\helpers\Url;
 
 /* @var $this yii\web\View */
 /* @var $content string */
+
+//AppAsset::register($this);
+AppAssetAdmin::register($this);
+BootstrapAsset::register($this);
 ?>
 
 
@@ -21,39 +27,39 @@ use yii\helpers\Url;
         <meta http-equiv="content-type" content="text/html; charset=utf-8" />
         <meta http-equiv="Expires" content="Tue, 01 Jan 1980 1:00:00 GMT" />
         <meta http-equiv="Pragma" content="no-cache" />
-        <link rel="stylesheet" href="/public/reset-fonts.css" type="text/css" media="screen, projection" />
-        <link rel="stylesheet" href="/public/gt-styles.css" type="text/css" media="screen, projection" />
+
         <?php $this->head() ?>
     </head>
     <body>
     <?php $this->beginBody() ?>
 
-     <!-- head -->
-    <div class="gt-hd clearfix">
-        <!-- logo -->
-        <div class="gt-logo">
-            Admin panel
-        </div>
-        <!-- / logo -->
+    <div class="container">
+        <!-- head -->
+        <div class="gt-hd clearfix">
+            <!-- logo -->
+            <div class="gt-logo">
+                Admin panel
+            </div>
+            <!-- / logo -->
 
-        <!-- navigation -->
-        <div class="gt-nav">
-            <ul>
-                <li><a href="<?= Url::to(['/admin/index']) ?>">Home</a></li>
-                <li><a href="<?= Url::to(['/admin/blog']) ?>">Blog</a></li>
-            </ul>
-        </div>
-        <!-- / navigation -->
+            <!-- navigation -->
+            <div class="gt-nav">
+                <ul>
+                    <li><a href="<?= Url::to(['/admin/index']) ?>">Home</a></li>
+                    <li><a href="<?= Url::to(['/admin/blog']) ?>">Blog</a></li>
+                </ul>
+            </div>
+            <!-- / navigation -->
 
-    </div>
-    <!-- / head -->
+        </div>
+        <!-- / head -->
         <?= $content ?>
         <!-- footer -->
-    <div class="gt-footer">
-        <p>Copyright &copy; 2016 maesh.kyryll@gmail.com</p>
+        <div class="gt-footer">
+            <p>Copyright &copy; 2016 maesh.kyryll@gmail.com</p>
+        </div>
+        <!-- /footer -->
     </div>
-    <!-- /footer -->
-
 
     <?php $this->endBody() ?>
     </body>
