ee9bfa57636e3ee839debc85e9e9a3615301c210
igor <dp030988kie@gmail.com>
2016-02-08 23:56:17 +0200

    create module ADMIN

--- config/web.php
+++ config/web.php
@@ -6,7 +6,13 @@ $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
+    'modules' => [
+        'admin' => [
+            'class' => 'app\modules\admin\AdminModule',
+        ],
+    ],
     'components' => [
+
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'KkXuxnL1Cfw6CppUIk6jlIwtk6Xt4pXM',
